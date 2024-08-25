98a258770ef1c5df522201d382decbd3eb24df18
Shiva-04z <141862114+Shiva-04z@users.noreply.github.com>
2024-08-25 17:21:36 +0530

    Second Comit registerd to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:irctc/features/splashscreen/splash_screen_bindings.dart';
 import 'package:irctc/navigation/get_pages_constant.dart';
 
 import 'features/splashscreen/splash_screen_view.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
