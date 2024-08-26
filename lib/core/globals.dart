74ff218c7225db3ef1ab38f2ddb33c64113f9416
Shiva-04z <141862114+Shiva-04z@users.noreply.github.com>
2024-08-26 12:35:11 +0530

    Fourth Commit (Global Dart)

---
+++ lib/core/globals.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+import '../navigation/routes_constant.dart';
+
+RxBool isLogin = true.obs;
+RxBool isProfileUpdated = false.obs;
+RxString uploadedImages = "".obs;
+Rx<String> passengerDoc = "".obs;
+RxString selected = "".obs;
+RxBool isAdmin = false.obs;
+
+loginCheck() {
+  if (isLogin.value) {
+    Get.offNamed(RoutesConstant.homePage);
+  } else {
+    Get.offNamed(RoutesConstant.loginPage);
+  }
+}
