4c698eb2509dc428a6f8f0a955662cf88cf63713
Shiva-04z <141862114+Shiva-04z@users.noreply.github.com>
2024-08-27 23:55:34 +0530

    Sixth Commit Added Some icons

--- lib/features/homescreen/home_screen_controller.dart
+++ lib/features/homescreen/home_screen_controller.dart
@@ -1,9 +1,13 @@
 import 'package:get/get.dart';
 
+import '../../navigation/routes_constant.dart';
+
 class HomeScreenController extends GetxController
 {
   to()
-  {}
+  {
+    Get.offAllNamed(RoutesConstant.loginPage);
+  }
 
 
 }
